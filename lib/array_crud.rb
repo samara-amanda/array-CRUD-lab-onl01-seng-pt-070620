4384779481c1c12ef80ee8b1c9fc25c7c3e50aad
Samara Grant <samara.amanda01@gmail.com>
2020-07-11 16:47:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,6 @@ end
 
 def add_element_to_end_of_array(array, element)
   array.push(element)
-
 end
 
 def add_element_to_start_of_array(array, element)
@@ -17,11 +16,9 @@ end
 
 def remove_element_from_end_of_array(array)
   array.pop
-
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
